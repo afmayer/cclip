ed68d2f78527e0679e4133e37f18560b029eb4a0
Alexander F. Mayer <tinybluebutton@gmail.com>
2013-06-24 21:10:01 +0200

    Auto resize input buffer in steps of 4096 bytes, free() afterwards.

--- cclip.c
+++ cclip.c
@@ -53,12 +53,14 @@ int main(int argc, char *argv[])
     char *pInputBuffer;
     wchar_t *pUnicodeBuffer;
     unsigned int inputBufferSize;
+    unsigned int inputBufferSizeStep;
     unsigned int unicodeBufferSize;
     unsigned int totalReadBytes;
     unsigned int codepage;
     int retval;
 
-    inputBufferSize = 4096; // TODO buffer size command line switch
+    inputBufferSize = 0;
+    inputBufferSizeStep = 4096; // TODO buffer size step command line switch
 
     if (standardin == INVALID_HANDLE_VALUE)
     {
@@ -71,21 +73,30 @@ int main(int argc, char *argv[])
     //   FILE_TYPE_CHAR --> invoked as cclip.exe
     //   FILE_TYPE_PIPE --> invoked as someprocess.exe | cclip.exe
 
-    codepage = GetConsoleCP(); // TODO command line switch for input codepage override
+    codepage = GetConsoleCP();
+    // TODO use codepage = GetACP() when reading from a file
+    // TODO command line switch for input codepage override
+    //   allow default codepage identifiers CP_ACP, CP_OEMCP, ?CP_MACCP?,
+    //   CP_THREAD_ACP, CP_SYMBOL, CP_UTF7 and CP_UTF8
 
-    pInputBuffer = malloc(inputBufferSize);
-    if (pInputBuffer == NULL)
-    {
-        fprintf(stderr, "Could not allocate input buffer\n");
-        exit(1);
-    }
-
+    pInputBuffer = NULL;
     totalReadBytes = 0;
     while (1)
     {
         unsigned int readbytes;
+        char *pReadPointer;
+
+        pInputBuffer = realloc(pInputBuffer, inputBufferSize + inputBufferSizeStep);
+        pReadPointer = pInputBuffer + inputBufferSize;
+        inputBufferSize += inputBufferSizeStep;
+        if (pInputBuffer == NULL)
+        {
+            fprintf(stderr, "Could not allocate input buffer\n");
+            exit(1);
+        }
+
         // TODO double buffer size for each call and read everyting into one buffer
-        if (!ReadFile(standardin, pInputBuffer, inputBufferSize, &readbytes, NULL))
+        if (!ReadFile(standardin, pReadPointer, inputBufferSizeStep, &readbytes, NULL))
         {
             unsigned int err = GetLastError();
             if (err == ERROR_BROKEN_PIPE && GetFileType(standardin) == FILE_TYPE_PIPE)
@@ -95,6 +106,7 @@ int main(int argc, char *argv[])
             else
             {
                 fprintf(stderr, "ReadFile() failed, GetLastError() = 0x%X\n", GetLastError());
+                free(pInputBuffer);
                 exit(1);
             }
         }
@@ -110,6 +122,7 @@ int main(int argc, char *argv[])
     if (pUnicodeBuffer == NULL)
     {
         fprintf(stderr, "Could not allocate Unicode conversion buffer\n");
+        free(pInputBuffer);
         exit(1);
     }
 
@@ -118,8 +131,11 @@ int main(int argc, char *argv[])
     if (retval == 0)
     {
         fprintf(stderr, "MultiByteToWideChar() failed, GetLastError() = %X\n", GetLastError());
+        free(pInputBuffer);
         exit(1);
     }
+
+    free(pInputBuffer);
 
     retval = WriteToClipboard(CF_UNICODETEXT, pUnicodeBuffer, retval * sizeof(wchar_t));
     if (retval != 0)
